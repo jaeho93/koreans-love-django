bf0f373f090201b0b897950778624f5223d8ba27
Jaeho Cho <jaeho8032@gmail.com>
2018-09-21 22:13:11 +0900

    모델을 단순하게 보여주는 admin 구현
    - 각 admin.py에 모델 등록
    
     Changes to be committed:
    	modified:   accounts/admin.py
    	modified:   board/admin.py
    	modified:   contents/admin.py

--- contents/admin.py
+++ contents/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import DjangoUser, Company, Community, Project, Blog
 
-# Register your models here.
+
+admin.site.register(DjangoUser)
+admin.site.register(Company)
+admin.site.register(Community)
+admin.site.register(Project)
+admin.site.register(Blog)
