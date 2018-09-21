6d14b99859331d225df6350ea186c1930545a4b6
Jaeho Cho <jaeho8032@gmail.com>
2018-09-21 22:07:57 +0900

    contents 앱 views 함수 pagination 적용한 views 함수 작성
    - infinite scroll 사용 여부 보류로 코드 삭제
    
    - pagination만 적용한  단순 list view로 구현
    
    - Archive -> Blog로 모델 명 변경 반영
    
     Changes to be committed:
    	modified:   contents/views.py

--- contents/views.py
+++ contents/views.py
@@ -1,21 +1,49 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render
+
+from . import models
 
 
 def user_list(request):
-    pass
+    all_users = models.DjangoUser.objects.all()
+    paginator = Paginator(all_users, 10)
+
+    page = request.GET.get('page', 1)
+    users = paginator.get_page(page)
+    return render(request, 'contents/user_list.html', {'users': users})
 
 
 def company_list(request):
-    pass
+    all_companies = models.Company.objects.all()
+    paginator = Paginator(all_companies, 10)
+
+    page = request.GET.get('page', 1)
+    companies = paginator.get_page(page)
+    return render(request, 'contents/company_list.html', {'companies': companies})
 
 
 def community_list(request):
-    pass
+    all_communities = models.Community.objects.all()
+    paginator = Paginator(all_communities, 10)
+
+    page = request.GET.get('page', 1)
+    communities = paginator.get_page(page)
+    return render(request, 'contents/community_list.html', {'communities': communities})
 
 
 def project_list(request):
-    pass
+    all_projects = models.Project.objects.all()
+    paginator = Paginator(all_projects, 10)
+
+    page = request.GET.get('page', 1)
+    projects = paginator.get_page(page)
+    return render(request, 'contents/project_list.html', {'projects': projects})
 
 
-def archive_list(request):
-    pass
+def blog_list(request):
+    all_blogs = models.Blog.objects.all()
+    paginator = Paginator(all_blogs, 10)
+
+    page = request.GET.get('page', 1)
+    blogs = paginator.get_page(page)
+    return render(request, 'contents/blog_list.html', {'blogs': blogs})
